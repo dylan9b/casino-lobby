fcd6bea51f177368eceed827f2badc354de996be
dylan9b <dylan9b@gmail.com>
2025-07-18 09:56:36 +0200

    Added clear search input

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -20,16 +20,33 @@ function Search() {
     setFilter({ ...filter, searchTerm: inputValue });
   };
 
+  const handleClearSearch = () => {
+    setInputValue("");
+    setFilter({ ...filter, searchTerm: "" });
+  };
+
   return (
     <form onSubmit={handleSubmit} className="mb-4">
-      <input
-        type="text"
-        name="searchTerm"
-        value={inputValue}
-        placeholder="Type and press Enter"
-        className="border rounded border-gray-200 px-8 py-2 w-full focus-visible:outline-0 focus-visible:ring-4 focus-visible:ring-teal-600/10"
-        onChange={handleOnChange}
-      />
+      <div className="relative">
+        <input
+          type="text"
+          name="searchTerm"
+          value={inputValue}
+          placeholder="Type and press Enter"
+          className="border rounded border-gray-200 px-8 py-2 w-full focus-visible:outline-0 focus-visible:ring-4 focus-visible:ring-teal-600/10"
+          onChange={handleOnChange}
+        />
+        <button
+          type="button"
+          className={` ${
+            !inputValue ? "opacity-40 cursor-not-allowed" : "cursor-pointer"
+          }  absolute right-4 top-1/2 -translate-y-1/2 text-2 xl`}
+          onClick={handleClearSearch}
+          disabled={!inputValue}
+        >
+          &times;
+        </button>
+      </div>
     </form>
   );
 }
--- src/pages/games.jsx
+++ src/pages/games.jsx
@@ -58,7 +58,7 @@ function Games() {
 
       {/* No games found */}
       {!isLoading && !games.length && (
-        <div className="p-2 w-full rounded border-b-amber-400 text-amber-800 bg-amber-400/30 ring-2 ring-offset-2 ring-amber-200 text-[clamp(1.25rem,1rem,1.9rem)]">
+        <div className="p-2 w-full rounded border-b-amber-400 text-amber-800 bg-amber-400/30 ring-2 ring-offset-2 ring-amber-200 text-sm md:text-base">
           <p className="m-0">No games found with this filter.</p>
           <small className="m-0">Please try with something else.</small>
         </div>
