49edbe4f157895ed63c89b8f87ce918cd467f562
dylan9b <dylan9b@gmail.com>
2025-07-20 15:05:36 +0200

    Updated warning messages design for when no games are found.

--- src/components/no-favourites.jsx
+++ src/components/no-favourites.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NoFavourites() {
   return (
-    <div className="p-2 w-full rounded border-b-amber-400 text-amber-800 bg-amber-400/30 ring-2 ring-offset-2 ring-amber-200 text-sm md:text-base">
+    <div className="p-2 w-full text-center rounded border-b-amber-400 text-amber-800 bg-amber-400/30 ring-2 ring-offset-2 ring-amber-200 text-sm md:text-base">
       <p className="m-0">No games found.</p>
       <small className="m-0">
         Please click
--- src/components/no-games.jsx
+++ src/components/no-games.jsx
@@ -1,8 +1,10 @@
 function NoGames() {
   return (
-    <div className="p-2 w-full rounded border-b-amber-400 text-amber-800 bg-amber-400/30 ring-2 ring-offset-2 ring-amber-200 text-sm md:text-base">
-      <p className="m-0">No games found with this filter.</p>
-      <small className="m-0">Please try with something else.</small>
+    <div className="px-0.5 w-full">
+      <div className="p-2 w-full rounded border-b-amber-400 text-amber-800 bg-amber-400/30 ring-2 ring-offset-2 ring-amber-200 text-sm md:text-base text-center">
+        <p className="m-0">No games found with this filter.</p>
+        <small className="m-0">Please try with something else.</small>
+      </div>
     </div>
   );
 }
