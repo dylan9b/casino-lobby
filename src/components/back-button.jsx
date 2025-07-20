0324f320d61ef17f7e68052eaa4fb3d56cb900ad
dylan9b <dylan9b@gmail.com>
2025-07-20 13:26:42 +0200

    Updated functionality for back button to redirect to home page.

--- src/components/back-button.jsx
+++ src/components/back-button.jsx
@@ -1,19 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function BackButton() {
-  const navigate = useNavigate();
-
-  const handleOnClick = () => {
-    navigate(-1);
-  };
-
   return (
-    <button
-      onClick={handleOnClick}
-      className="cursor-pointer w-4 h-4 p-4 text-xl rounded-full border border-gray-200 bg-gray-200 text-black flex items-center justify-center"
+    <Link
+      to={"/"}
+      className="cursor-pointer w-4 h-4 p-4 text-xl rounded-full border border-gray-200 bg-gray-200 text-gray-600 flex items-center justify-center duration-300 transition-all hover:scale-110 ring-2 ring-gray-400/30 focus"
     >
       &lsaquo;
-    </button>
+    </Link>
   );
 }
 
